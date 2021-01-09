c00b9ae48f3a8b3a1ca3880ee4bf51802b8efb42
Masud8851 <76719164+Masud8851@users.noreply.github.com>
2021-01-09 18:22:35 +0600

    Add files via upload

---
+++ Logic 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+    int n=-70;
+    if (n<0)
+    {
+    printf("The number is negative\n");
+    }
+    else if (n>0)
+    {
+    printf("The number is positive\n");
+    }
+    else if (n==0)
+    {
+    printf("The number is 0!\n");
+    }
+    return 0;
+}
