08014a1ed08665fc3028173c0903f94f28f2355e
Masud8851 <76719164+Masud8851@users.noreply.github.com>
2021-01-26 23:34:39 +0600

    Add files via upload

---
+++ 6 a is greater than be.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main ()
+{
+int a;
+int b;
+
+printf("Enter the First number :");
+scanf("%d",&a);
+
+printf("Enter the Second number :");
+scanf("%d",&b);
+
+
+if(a>b)
+{
+printf("First\n");
+}
+}
