6675d2cdb5cee486b0abe8e52c8feebc1aa47ef4
Masud8851 <76719164+Masud8851@users.noreply.github.com>
2021-01-29 23:26:46 +0600

    Add files via upload

---
+++ Function 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void introduction ()
+{
+    printf("Hi!\n");
+    printf("My name is Chaitanyo.\n");
+    printf("How are you?\n");
+}
+int main ()
+{
+    introduction ();
+    return 0;
+}
