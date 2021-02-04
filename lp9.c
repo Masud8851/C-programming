71acec9b0087b8be5372585e3e713215b2f5f684
Masud8851 <76719164+Masud8851@users.noreply.github.com>
2021-02-04 23:37:33 +0600

    Add files via upload

---
+++ lp9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+    int i,j,m=5,n=4;
+    for(i=0;i<n;i++)
+
+    {
+        for (j=0;j<m;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
