048f56984a3e6ee9115ee52a2e574df10e164068
Masud8851 <76719164+Masud8851@users.noreply.github.com>
2021-02-13 23:01:07 +0600

    Add files via upload

---
+++ T-2.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+int main ()
+{
+    int i,j;
+
+    printf("Enter the First number : ");
+    scanf("%d",&i);
+    printf("Enter the Second number : ");
+    scanf("%d",&j);
+
+    if(i>j)
+        {
+            printf("First is greater.");
+        }
+        else{
+                printf("First is not greater.");
+            }
+    return 0;
+}
