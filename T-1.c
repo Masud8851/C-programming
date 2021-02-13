20d8adfe4caad085e9010a0082931fc85687069e
Masud8851 <76719164+Masud8851@users.noreply.github.com>
2021-02-13 23:00:46 +0600

    Add files via upload

---
+++ T-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+    int i,j;
+
+    printf("Enter the First number : ");
+    scanf("%d",&i);
+    printf("Enter the Second number : ");
+    scanf("%d",&j);
+    if(i>j)
+        {
+            printf("First");
+        }
+        else{
+                printf("Second");
+            }
+    return 0;
+}
